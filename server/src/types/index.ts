fa85653b58c7ef52bafe929ff6a9c98b644c6e80
Ali Nisar Ahmed <ali.nisar87@gmail.com>
2021-10-21 20:47:21 -0600

    add types on server

--- server/src/types/index.ts
+++ server/src/types/index.ts
@@ -1,6 +1,71 @@
-export interface LocationWithTimezone {
-  location: string;
-  timezoneName: string;
-  timezoneAbbr: string;
-  utcOffset: number;
+export const allCurrencies = [
+  "USD",
+  "CAD",
+  "CNY",
+  "EUR",
+  "JPY",
+  "INR",
+  "GBP",
+  "AUD",
+  "SGD",
+  "AED",
+];
+
+export enum CurrencyCode {
+  USD = "USD",
+  CAD = "CAD",
+  CNY = "CNY",
+  EUR = "EUR",
+  JPY = "JPY",
+  INR = "INR",
+  GBP = "GBP",
+  AUD = "AUD",
+  SGD = "SGD",
+  AED = "AED",
 }
+
+// export type CurrencyCode = typeof allCurrencies[number];
+
+export type CurrencyAPIResponse = {
+  meta: {
+    code: number;
+    disclaimer: string;
+  };
+  response: {
+    date: string;
+    base: string;
+    rates: {
+      [key in CurrencyCode]: number;
+    };
+  };
+};
+
+export interface NetWorth {
+  assets: Asset;
+  liabilities: Liability;
+}
+
+export interface Asset {
+  cashAndInvestments: LineItem[];
+  longTermAssets: LineItem[];
+}
+
+export interface Liability {
+  shortTerm: LineItem[];
+  longTerm: LineItem[];
+}
+
+export interface LineItem {
+  label: string;
+  amount: string;
+}
+
+export interface AppState {
+  netWorth: NetWorth;
+}
+
+export type NetWorthConvertRequest = {
+  oldCurrency: CurrencyCode;
+  newCurrency: CurrencyCode;
+  netWorth: NetWorth;
+};
