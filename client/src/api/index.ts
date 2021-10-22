e5b17f781c65d46797d2351eb48be15de384ce8b
Ali Nisar Ahmed <ali.nisar87@gmail.com>
2021-10-22 11:49:51 -0600

    api call from the client to the server

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,8 +1,19 @@
-import axios from 'axios';
+import axios from "axios";
+import { ConvertNetWorthRequest, CurrencyCode, NetWorth } from "../types";
 
-const serverUrl = 'http://localhost:8080'
+const serverUrl = process.env.REACT_APP_API_URL;
+console.log("serverurl: ", serverUrl);
 
-// export const fetchTimezones = async (): Promise<LocationWithTimezone[]> => {
-//   const res = await axios.get(`/api/timezones`);
-//   return res.data;
-// }
+export async function convertNetWorth({
+  oldCurrency,
+  newCurrency,
+  netWorth,
+}: ConvertNetWorthRequest): Promise<NetWorth> {
+  const res = await axios.post(`${serverUrl}/networth`, {
+    oldCurrency,
+    newCurrency,
+    netWorth,
+  });
+
+  return res.data as NetWorth;
+}
