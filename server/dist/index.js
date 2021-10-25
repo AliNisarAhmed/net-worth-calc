2390ed0b7033bf8f0eec37293d899d115b881234
Ali Nisar Ahmed <ali.nisar87@gmail.com>
2021-10-25 13:47:21 -0600

    move index.js to src

--- server/dist/index.js
+++ server/dist/index.js
@@ -1,10 +1,11 @@
-const express = require("express");
-const app = express();
-const port = 8080;
-app.get("/", (req, res) => {
-    res.send("Hello world!");
-});
-app.listen(port, () => {
-    console.log(`server started at http://localhost:${port}`);
-});
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const app_1 = __importDefault(require("./app"));
+const port = process.env.PORT || 3000;
+(0, app_1.default)().then((app) => app.listen(port, () => {
+    console.log("Listening on port: ", port);
+}));
 //# sourceMappingURL=index.js.map
