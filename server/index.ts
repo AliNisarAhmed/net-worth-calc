29976ac606ca2f32e014e1f7b407abd3d7f0acd1
Ali Nisar Ahmed <ali.nisar87@gmail.com>
2021-10-25 13:32:38 -0600

    update PORT

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,9 @@
 import app from "./src/app";
 
-const { PORT, API_KEY } = process.env;
+const port = process.env.PORT || 3000;
 
 app().then((app) =>
-  app.listen(PORT, () => {
-    console.log("Listening on port: ", PORT);
+  app.listen(port, () => {
+    console.log("Listening on port: ", port);
   })
 );
