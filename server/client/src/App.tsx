d7d2b3cd0ae9491d49d1d9a613882cfbc74e8230
Ali Nisar Ahmed <ali.nisar87@gmail.com>
2021-10-27 13:33:35 -0600

    FE: lazy loading components

--- server/client/src/App.tsx
+++ server/client/src/App.tsx
@@ -1,5 +1,4 @@
 import Assets from "./components/Assets";
-import Liabilities from "./components/Liabilities";
 import { FormStateContextProvider } from "./context/FormStateContext";
 import CurrencySelector from "./components/CurrencySelector";
 import NetWorth from "./components/NetWorth";
@@ -8,7 +7,10 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { AppContextProvider } from "./context/AppStateContext";
 import Header from "./components/Header";
-import Footer from "./components/Footer";
+import { lazy, Suspense } from "react";
+
+const Footer = lazy(() => import("./components/Footer"));
+const Liabilities = lazy(() => import("./components/Liabilities"));
 
 function App() {
   return (
@@ -29,12 +31,16 @@ function App() {
             <form>
               <NetWorth />
               <Assets />
-              <Liabilities />
+              <Suspense fallback={<p>Loading...</p>}>
+                <Liabilities />
+              </Suspense>
             </form>
           </AppContextProvider>
         </FormStateContextProvider>
       </Layout>
-      <Footer />
+      <Suspense fallback={<p>Loading...</p>}>
+        <Footer />
+      </Suspense>
     </>
   );
 }
